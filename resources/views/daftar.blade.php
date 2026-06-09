7609e2bd6e39eac1aece25cc6ed9eb72521739b6
Dana <57206216+ffigoperdana@users.noreply.github.com>
2026-06-09 09:24:05 +0700

    update logo

--- resources/views/daftar.blade.php
+++ resources/views/daftar.blade.php
@@ -4,12 +4,11 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="dicoding:email" content="raja.pasha.azfs@upi.edu">
     <link rel="stylesheet" href="{{asset('/css/style.css')}}">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="icon" href="{{asset('/images/logo.png')}}" type="image/png"><title>Plasmo</title>
+    <link rel="icon" href="{{asset('/images/logo-favicon.png')}}" type="image/png"><title>Plasmo</title>
   </head>
   <body>
     <header class="container">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,12 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="dicoding:email" content="raja.pasha.azfs@upi.edu">
     <link rel="stylesheet" href="{{asset('/css/style.css')}}">
     <link rel="stylesheet" href="{{asset('/css/responsive.css')}}">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <link rel="icon" href="{{asset('/images/logo.png')}}" type="image/png">
+    <link rel="icon" href="{{asset('/images/logo-favicon.png')}}" type="image/png">
     <title>Plasmo</title>
   </head>
   <body>
